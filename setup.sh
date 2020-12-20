f58bb5e43d6209c5d74fa9fc127ca73039fdb4b8
zakattack9 <zakattack26111@gmail.com>
2020-12-20 13:58:44 -1000

    added setup script and fixed yaml naming

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# applies Flannel CNI, Metal Load Balancer, and Ingress-Nginx Controller
+kubectl apply -f ./flannel/flannel.yaml
+kubectl apply -f ./metallb/metallb-namespace.yaml
+kubectl apply -f ./metallb/metallb.yaml
+kubectl apply -f ./metallb/metallb-config.yaml
+kubectl apply -f ./ingress-nginx/ingress-nginx-baremetal.yaml
